dfb4c5e86ccef82b3b52d436f2cfe39a035f6148
root <anon@ubicoders.com>
2024-08-15 05:59:07 +0000

    update

---
+++ aruco_ps/global_aruco_utils.py
@@ -0,0 +1,68 @@
+import numpy as np
+
+def check_nan(x):
+    if np.isnan(x).any() or np.isinf(x).any() or np.isneginf(x).any():
+        return True
+    return False
+
+def check_nan_list(x):
+    for i in x:
+        if check_nan(i):
+            return True
+    return False
+
+def check_nan_np(x):
+    if np.isnan(x).any() or np.isinf(x).any() or np.isneginf(x).any():
+        return True
+    return False
+
+
+def cveul_aaeeul(cveul):
+    aaeeul = np.zeros(3)
+    aaeeul[0] = cveul[2]
+    aaeeul[1] = cveul[0]
+    aaeeul[2] = -cveul[1]
+    return aaeeul
+
+def eul2rotm(eul):
+    """
+    Convert Euler angles to rotation matrix.
+
+    Parameters:
+    - eul: Euler angles (roll, pitch, yaw) in radians
+
+    Returns:
+    - R: Rotation matrix ZYX
+    """
+    roll, pitch, yaw = eul[0], eul[1], eul[2]
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(roll), -np.sin(roll)],
+                    [0, np.sin(roll), np.cos(roll)]])
+    R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
+                    [0, 1, 0],
+                    [-np.sin(pitch), 0, np.cos(pitch)]])
+    R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
+                    [np.sin(yaw), np.cos(yaw), 0],
+                    [0, 0, 1]])
+    R_zyx = np.dot(R_z, np.dot(R_y, R_x))
+    return R_zyx
+
+def cvXYZ_aaeXYZ(cvXYZ):
+    aaeXYZ = np.zeros(3)
+    try:
+        aaeXYZ[0] = cvXYZ[2]
+        aaeXYZ[1] = cvXYZ[0]
+        aaeXYZ[2] = -cvXYZ[1]
+    except:
+        aaeXYZ = np.array([0., 0., 0.])
+    return aaeXYZ
+
+def get_aae_T(cveul, cvXYZ):
+
+    aaeXYZ = cvXYZ_aaeXYZ(cvXYZ)
+    aaeeul = cveul_aaeeul(cveul)
+    R = eul2rotm(aaeeul)
+    T = np.eye(4)
+    T[:3, :3] = R
+    T[:3, 3] = aaeXYZ
+    return T
--- aruco_ps/psnode_global_aruco.py
+++ aruco_ps/psnode_global_aruco.py
@@ -5,6 +5,9 @@ import numpy as np
 from .perf_utils import TimerTicTok
 from aruco_interface.msg import ImageMarkers
 from rclpy.qos import QoSProfile
+from .global_aruco_utils import get_aae_T,check_nan_np
+# format numpy to print all 3 decimals
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
 class GlobalArucoPSNode(Node):
     def __init__(self):
@@ -21,12 +24,43 @@ class GlobalArucoPSNode(Node):
                 qos_profile)
         
     def handle_aruco(self, msg):
+        self.tictok.update()
+        self.tictok.pprint()
+
         aruco_marker_list = msg.aruco_markers_0
+        eul_list = []
+        xyz_list = []
+        tvec_list = []
         for marker in aruco_marker_list:
             mid = marker.mid
-            eul = ""
-            xyz = marker.xyz
-            self.get_logger().info(f"Marker ID: {mid}, 3D coordinates: {xyz.x}, {xyz.y}, {xyz.z}")
+            eul = np.array([marker.eul.x, marker.eul.y, marker.eul.z])
+            xyz = np.array([marker.pc_xyz.x, marker.pc_xyz.y, marker.pc_xyz.z])
+            tvec = np.array([marker.tvec.x, marker.tvec.y, marker.tvec.z])
+            if (check_nan_np(eul) or check_nan_np(xyz) or check_nan_np(tvec)):
+                print(f"Error: NaN detected in marker {mid}")
+                continue
+           
+            eul_list.append(eul)
+            xyz_list.append(xyz)
+            tvec_list.append(tvec)
+
+
+        # calcualte average
+        if (len(eul_list) == 0 or len(xyz_list) == 0 or len(tvec_list) == 0):
+            eul_avg = np.array([0., 0., 0.])
+            xyz_avg = np.array([0., 0., 0.])
+            tvec_avg = np.array([0., 0., 0.])
+        eul_avg = np.mean(np.array(eul_list), axis=0)
+        xyz_avg = np.mean(np.array(xyz_list), axis=0)
+        tvec_avg = np.mean(np.array(tvec_list), axis=0)
+
+        if (check_nan_np(eul_avg) or check_nan_np(xyz_avg) or check_nan_np(tvec_avg)):
+            print(f"Error: NaN detected in average")
+            aaeT = np.eye(4)
+        else:
+            aaeT = get_aae_T(eul_avg, xyz_avg)
+        self.get_logger().info(f"Transform: \n{aaeT}")
+            
     
 
 
